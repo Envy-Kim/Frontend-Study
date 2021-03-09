d36c1c85f617cb72f896e4b8d303def1c876febc
김경식 <envy7937@gmail.com>
2021-03-09 10:25:15 +0900

    Add  todo

--- 1st/custom_modal.js
+++ 1st/custom_modal.js
@@ -1,29 +1,81 @@
+/*
+    커스텀 엘리먼트
+    쉐도우돔
+    슬롯 (or template)
+    이용하여 Custom modal 작성
+ */
+class SimpleModal extends HTMLElement {
 
-class CustomModal extends HTMLElement {
     constructor() {
         super();
 
         this.attachShadow({mode: "open"});
         this._visible = false;
 
-        /** CustomModal의 Template */
         this.shadowRoot.innerHTML = `
-            <div class="modal">
+            <div class="modal-wrap">
                 <div class="modal-box">
-                    <div class="modal-head">
-                        <p>
-                            <slot name="title" />
-                        </p>
-                        <span class="modal-close">&times;</span>
+                    <span class="close">&times;</span>
+                    <div class="modal-title">
+                        <slot name="title"></slot>
                     </div>
-                    <div class="modal-content">
-                        <p class="modal-subtit">
-                            <slot name="sub-title" />
-                        </p>
-                        <slot name="content" />
+                    <div class="modal-body">
+                        <slot name="content">test</slot>
                     </div>
                 </div>
             </div>
+            
+            <style>
+                .modal-wrap {
+                    display: none;
+                    position: fixed;
+                    background-color: rgb(0, 0, 0);
+                    background-color: rgba(0, 0, 0, 0.4);
+                    z-index: 1;
+                    left: 0;
+                    top: 0;
+                    width: 100%;
+                    height: 100%;
+                    overflow: auto;
+                }
+    
+                .modal-box {
+                    background-color: #efefef;
+                    margin: 15% auto;
+                    padding: 20px;
+                    border: 1px solid #888;
+                    width: 50%;
+                    border-radius: 15px;
+                    -webkit-animation-name: animatetop;
+                    -webkit-animation-duration: 1s;
+                    animation-name: animatetop;
+                    animation-duration: 0.4s;
+                }
+    
+                .close {
+                    color: #aaa;
+                    float: right;
+                    font-size: 28px;
+                    font-weight: bold;
+                }
+    
+                .close:hover,
+                .close:focus {
+                    color: black;
+                    text-decoration: none;
+                    cursor: pointer;
+                }
+                
+                @-webkit-keyframes animatetop {
+                    from {top: -300px; opacity: 0}
+                    to {top: 0; opacity: 1}
+                }
+                
+                @keyframes animatetop {
+                    from {top: -300px; opacity: 0}
+                    to {top: 0; opacity: 1}
+                }
+            </style>
         `;
     }
 
@@ -31,24 +83,28 @@ class CustomModal extends HTMLElement {
      * 가상 트리가 document 에 연결된후 콜백
      */
     connectedCallback() {
-        /** custom element 에서 바인딩한 속성을 주입 */
-        if (this.getAttribute('id'))
-            this.shadowRoot.querySelector('.custom-btn').setAttribute('id', this.getAttribute('id'));
+        this._modal = this.shadowRoot.querySelector('.modal-wrap');
+        this.shadowRoot.querySelector('.close').addEventListener('click', this._hide.bind(this));
 
-        if (this.getAttribute('class'))
-            this.shadowRoot.querySelector('.custom-btn').setAttribute('id', this.getAttribute('class'));
-
-        if (this.getAttribute('name'))
-            this.shadowRoot.querySelector('.custom-btn').setAttribute('id', this.getAttribute('name'));
-
-        if (this.getAttribute('value'))
-            this.shadowRoot.querySelector('.custom-btn').setAttribute('id', this.getAttribute('value'));
+        this.shadowRoot.querySelector('.modal-box').style.backgroundColor
+            = this.getAttribute('bgcolor');
     }
 
     /**
      * 가상 트리가 document 에서 연결 해제 된 후 콜백
      */
     disconnectedCallback() {
+        this.shadowRoot.querySelector('.close').removeEventListener('click', this._hide);
+    }
+
+    _hide() {
+        this._visible = true;
+        this._modal.style.display = 'none';
+    }
+
+    show() {
+        this._visible = false;
+        this._modal.style.display = 'block';
     }
 
     /**
@@ -59,11 +115,9 @@ class CustomModal extends HTMLElement {
      * @param newValue
      */
     attributeChangedCallback(name, oldValue, newValue) {
+        /** todo: 요걸 사용해서 _hide(), show()를 바꿀 수 있지 않을까? */
         console.log('Custom square element attributes changed.');
-        updateStyle(this);
     }
-
-    /** javascript 객체는 private 속성을 지정할 수 없기 때문에 _ 를 속성명에 붙이는 것으로 private으로 간주 */
 }
 
-customElements.define('custom-modal', CustomModal);
+customElements.define('simple-modal', SimpleModal);
