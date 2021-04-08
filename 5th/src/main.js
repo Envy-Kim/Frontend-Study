f6ad2463f610a4a10d457449dfcf55397ec630a7
김경식 <envy7937@gmail.com>
2021-04-08 09:41:55 +0900

    Add: Vuex 설치

--- 5th/src/main.js
+++ 5th/src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import store from './store/index'
 
 // bootstrap 적용
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+// To use Vuex : Vuex는 ES2015문법을 사용한다.
+import 'es6-promise/auto'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
